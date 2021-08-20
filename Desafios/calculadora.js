511b2e1d228ce8819e7d087f455acfa35993671f
Jennifer Dominique <j.dominiqueds@gmail.com>
2021-08-20 19:28:59 -0300

    Atualizando a branch main

--- Desafios/calculadora.js
+++ Desafios/calculadora.js
@@ -2,50 +2,50 @@
 
 //Soma
 const soma = function (a, b){
-    return console.log(`${a} + ${b} = ${a + b}`);
+    return a + b;
 }
 
 //Subtracao
 const subtracao = (a, b) =>{
-    return console.log(`${a} - ${b} = ${a - b}`);
+    return a - b;
 }
 
 //Multiplicacao
-const multiplicacao = (a, b) =>{console.log(`${a} x ${b} = ${a * b}`)}
+const multiplicacao = (a, b) =>{
+    return a * b;
+}
 
 //Divisao
-const divisao = (a, b) =>{console.log(`${a} / ${b} = ${a / b}`)}
+const divisao = (a, b) =>{
+    return a / b;
+}
 
 //Resto da divisao
-const restoDivisao = (a, b) =>{console.log(`Resto de ${a} / ${b} = ${a % b}`)}
+const restoDivisao = (a, b) =>{
+    return a % b;
+}
 
 //Potencia
-const potencia = (a, b) =>{console.log(`${a} ^ ${b} = ${a ** b}`)}
+const potencia = (a, b) =>{
+    return a ** b;
+}
 
 //Porcentagem
 const porcentagem = (porcentagem, valor) =>{
-    return console.log(`${porcentagem}% de ${valor} = ${(valor * porcentagem) / 100}`);
+    return (valor * porcentagem) / 100;
 }
-
-//Maior que
-const maiorQue = (a, b) =>{console.log(`${a} > ${b} = ${a > b}`)}
-
-//Menor que
-const menorQue = (a, b) =>{console.log(`${a} < ${b} = ${a < b}`)}
-
-//Igual a 
-const igual = (a, b) =>{console.log(`${a} == ${b} = ${a == b}`)}
 
 //__________Execucoes e Resultados_______________
 console.log("_______Resultados________");
+r = 10
+e = 3
+s = 4
+p = 50
 
-soma(78, 15);
-subtracao(27, 13);
-multiplicacao(78, 2);
-divisao(80, 4);
-restoDivisao(50, 4);
-potencia(5, 2);
-porcentagem(50, 200);
-maiorQue(56, 65);
-menorQue(26, 47);
-igual(32, 23)
+console.log(`${e} + ${s} = ${soma(e, s)}`);
+console.log(`${p} - ${s} = ${subtracao(p, s)}`);
+console.log(`${s} x ${r} = ${multiplicacao(s, r)}`);
+console.log(`${p} / ${s} = ${divisao(p, s)}`);
+console.log(`Resto de ${p} / ${e} = ${restoDivisao(p, e)}`);
+console.log(`${s} ^ ${e} = ${potencia(s, e)}`);
+console.log(`${r} % de ${p} = ${porcentagem(r, p)}`);
